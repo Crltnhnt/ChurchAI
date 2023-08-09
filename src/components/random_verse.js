0851cf212b1a1050ee5400f63b315d9aab80e62a
Gr1ff3n44 <108370848+Gr1ff3n44@users.noreply.github.com>
2023-08-08 19:07:51 -0700

    added box button component

--- src/components/random_verse.js
+++ src/components/random_verse.js
@@ -1,1 +1,10 @@
-<><div>Hello world</div> <div>Courtland</div></>
+
+import React from 'react'
+import ReactDom from 'react-dom/client'
+
+function random_verse() {
+return <div><h1>Hello world</h1></div>;
+}
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(<random_verse />);
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { Button } from '@mui/material'
+import { Box } from '@mui/material'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import reportWebVitals from './reportWebVitals'
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <App />
+    <Box
+  display="flex"
+  justifyContent="center"
+  alignItems="center"
+  minHeight="100vh"> 
+      <Button
+      style={{
+        borderRadius: 35,
+        backgroundColor: "#21b6ae",
+        padding: "18px 36px",
+        fontSize: "18px"
+  }}
+  variant="contained"
+    >Touch Me Baby</Button>
+    </Box>
 
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
